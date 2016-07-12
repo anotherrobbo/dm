7dca291842aef41e396b235d39ad1cc32fd4d360
anotherrobbo <littlejerry7@hotmail.com>
2016-07-12 16:36:08 +0930

    more unicorn testing...

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,6 +1,6 @@
 # set path to application
 app_dir = File.expand_path("../..", __FILE__)
-shared_dir = "#{app_dir}/tmp"
+shared_dir = "#{app_dir}/../tmp"
 working_directory app_dir
 
 
@@ -10,7 +10,7 @@ preload_app true
 timeout 300
 
 # Set up socket location
-listen "#{shared_dir}/sockets/unicorn.sock", :backlog => 64
+#listen "#{shared_dir}/sockets/unicorn.sock", :backlog => 64
 #listen 3000
 
 # Logging
