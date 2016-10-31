04ebbb264423590f905db8853b1a48d6c3319fbf
anotherrobbo <littlejerry7@hotmail.com>
2016-11-01 10:13:23 +1030

    ...

--- buildpack_scripts/doextra.sh
+++ buildpack_scripts/doextra.sh
@@ -24,6 +24,7 @@ make install
 
 # gem install sqlite3
 echo Now installing sqlite3...
+ls ${rails_root}/vendor
 gem install sqlite3 -- --with-sqlite3-dir=${rails_root}/vendor/sqlite3
 gem list sqlite3
 
