4e06a70eb66e02686e93475bccd3ed19ce7d3ae9
anotherrobbo <littlejerry7@hotmail.com>
2016-10-31 16:32:48 +1030

    Put the sqlite3 path back

--- buildpack_scripts/doextra.sh
+++ buildpack_scripts/doextra.sh
@@ -24,7 +24,7 @@ make install
 # I should be able to gem install sqlite3 now
 echo Now installing sqlite3...
 cd ${rails_root}
-gem install sqlite3 -- --with-sqlite3-dir=./vendor/sqlite3
+gem install sqlite3 -- --with-sqlite3-dir=${rails_root}/vendor/sqlite3
 gem list sqlite3
 
 exit
